25237dbf5cce6ec7f5beca0c3f023f27a5453b44
SitHond <fms@sithond.ru>
2022-06-03 20:06:05 +0300

    Header v1.0

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Смотреть Аниме Онлайн</title>
+</head>
+<body>
+<header>
+    <div id="header-form">
+        <div class="posit">
+            <button class="btn" onclick="document.location='/anime.php'">Главнвя</button>
+            <button class="btn" onclick="document.location='/anime.php'">Аниме</button>
+            <button class="btn" onclick="document.location='/anime.php'">Манга</button>
+            <button class="btnn" onclick="document.location='/anime.php'">Случайное аниме</button></button>
+        </div>
+    </div>
+</header>
+</body>
+</html>
+
